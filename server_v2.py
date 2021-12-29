6f485796d6a0bd4fb449991ef8985d728e0ad957
Andrey-Nikitsin <the.niki.andrey@gmail.com>
2021-12-29 23:08:36 +0300

    теперь сообщения приходят по одному каждому пользователю

--- server_v2.py
+++ server_v2.py
@@ -2,11 +2,12 @@ from twisted.internet import reactor,endpoints, defer
 from twisted.internet.protocol import ServerFactory
 from twisted.protocols.basic import LineReceiver
 
+
 class ServerCL(LineReceiver):
     def __init__(self, users, addr):
         self.user_info = addr
         self.users = users
-
+    schet = 0
     def connectionMade(self):
         self.transport.write(b'Server connect\n')
         self.transport.write(b'inter you name\n')
@@ -14,10 +15,8 @@ class ServerCL(LineReceiver):
         print(self.users)
 
     def dataReceived(self, data):
-        # print(self.users)
         def SaveNameUser(NameUser):
             for key in self.users.keys():
-                # print(key)
                 if key == self:
                     self.users.pop(self)
                     self.users.update({self:(NameUser[:-2],self.user_info)})
@@ -27,19 +26,23 @@ class ServerCL(LineReceiver):
         def GeneralChat(messange):
             print(messange)
             for key in self.users.keys():
-                if len(self.users.get(self)) > 0:
+                if key != self:
                     name = self.users.get(self)[0]
                     key.transport.write(name)
                     key.transport.write(b':\t')
                     key.transport.write(messange)
 
-
         for key in self.users.keys():
             if len(self.users.get(key)) == 0:
                 SaveNameUser(data)
                 break
-            else:
-                GeneralChat(data)
+            elif len(self.users.get(self)) > 0:
+                self.schet+=1
+                print(self.schet)
+                if self.schet==len(self.users):
+                    GeneralChat(data)
+                    self.schet=0
+
 
 
 
@@ -50,5 +53,7 @@ class FactoryServerCl(ServerFactory):
     def buildProtocol(self, addr):
         return ServerCL(self.users, addr)
 
-endpoints.serverFromString(reactor, 'tcp:8051').listen(FactoryServerCl())
-reactor.run()
+
+if __name__ == '__main__':
+    endpoints.serverFromString(reactor, 'tcp:8052').listen(FactoryServerCl())
+    reactor.run()
