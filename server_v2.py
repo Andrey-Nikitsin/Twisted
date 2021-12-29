136d4fdfbc44d8744f65eb27fa3df82bded1ba48
Andrey-Nikitsin <the.niki.andrey@gmail.com>
2021-12-29 18:25:56 +0300

    добавляем список с данными пользователей

--- server_v2.py
+++ server_v2.py
@@ -1,23 +1,27 @@
-from twisted.internet import reactor,endpoints
-from twisted.internet.protocol import Protocol, Factory
+from twisted.internet import reactor,endpoints, defer
+from twisted.internet.protocol import ServerFactory
+from twisted.protocols.basic import LineReceiver
 
-class ServerCL(Protocol):
-    def __init__(self, addr):
-        self.addr = addr
+class ServerCL(LineReceiver):
+    def __init__(self,users):
+        self.users = users
 
     def connectionMade(self):
-        a = str(self.addr)
         self.transport.write(b'Server connect\n')
-        self.transport.write(a.encode("utf-8")+b'- you addr\n')
+        self.transport.write(b'write you name\n')
+
 
     def dataReceived(self, data):
-        data = data.decode('UTF-8')
-        print(data[:-2])
+        self.users.update({(data.decode("UTF-8"))[:-2]:self})
         self.transport.write(b'data received\n')
+        print(self.users)
 
-class FactoryServerCl(Factory):
+class FactoryServerCl(ServerFactory):
+    def __init__(self):
+        self.users = {}
+
     def buildProtocol(self, addr):
-        return ServerCL(addr)
+        return ServerCL(self.users)
 
 endpoints.serverFromString(reactor, 'tcp:8051').listen(FactoryServerCl())
 reactor.run()
